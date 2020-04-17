0bae17f0fb6c67e24e32f4a3c94f125a29e6f082
robynp108 <robynp108@gmail.com>
2020-04-16 21:36:02 -0500

    most of post request, researched and added uuid

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,16 @@
-var notes = require("../db/db.json");
+const notes = require("../db/db.json");
+const { v1: uuidv1 } = require("uuid");
 
 module.exports = function(app) {
     app.get("/api/notes", function(req, res) {
         res.json(notes);
       });  
 
+    app.post("/api/notes", function(req, res) {
+        console.log(req.body);
+        req.body.id = uuidv1();
+        notes.push(req.body);
+        // TODO write notes back to db.json
+        res.json(req.body);
+    })
 }
