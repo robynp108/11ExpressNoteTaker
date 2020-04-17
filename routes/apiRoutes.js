6cce198709dd6691374401f9a4d2062d1e219ccc
robynp108 <robynp108@gmail.com>
2020-04-17 16:16:42 -0500

    delete request code - functionality complete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,21 +1,34 @@
-const notes = require("../db/db.json");
+let notes = require("../db/db.json");
 const { v1: uuidv1 } = require("uuid");
 const fs = require('fs');
 
-module.exports = function(app) {
-    app.get("/api/notes", function(req, res) {
-        res.json(notes);
-      });  
+module.exports = function (app) {
+  app.get("/api/notes", function (req, res) {
+    res.json(notes);
+  });
 
-    app.post("/api/notes", function(req, res) {
-        console.log(req.body);
-        req.body.id = uuidv1();
-        notes.push(req.body);
-        // TODO write notes back to db.json
-        fs.writeFile('db.json', notes, function (err) {
-            if (err) throw err;
-            console.log('error');
-          });
-        res.json(req.body);
-    })
+  app.post("/api/notes", function (req, res) {
+    req.body.id = uuidv1();
+    notes.push(req.body);
+    fs.writeFile('./db/db.json', JSON.stringify(notes), function (err) {
+      if (err) throw err;
+    });
+    res.json(req.body);
+  });
+
+  app.delete('/api/notes/:id', function (req, res) {
+    let findId = (noteObject) => {
+      if (noteObject.id != req.params.id) {
+        return true
+      } else {
+        return false
+      }
+    }
+    notes = notes.filter(findId);
+    fs.writeFile('./db/db.json', JSON.stringify(notes), function (err) {
+      if (err) throw err;
+    });
+
+    res.send('Got a DELETE request at /api/notes/:id')
+  })
 }
