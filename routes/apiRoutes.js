8f67debb003203a3bfc33c604de106361d9828d0
robynp108 <robynp108@gmail.com>
2020-04-17 09:57:08 -0500

    wrote first post route, successfully writing and saving new notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,6 @@
 const notes = require("../db/db.json");
 const { v1: uuidv1 } = require("uuid");
+const fs = require('fs');
 
 module.exports = function(app) {
     app.get("/api/notes", function(req, res) {
@@ -11,6 +12,10 @@ module.exports = function(app) {
         req.body.id = uuidv1();
         notes.push(req.body);
         // TODO write notes back to db.json
+        fs.writeFile('db.json', notes, function (err) {
+            if (err) throw err;
+            console.log('error');
+          });
         res.json(req.body);
     })
 }
